dd29faccc927ef1c64417fa996a0283db048ec97
nurulhudda247 <nurulhudda247@gmail.com>
2024-08-09 19:22:10 +0600

    Day 1

---
+++ README.md
@@ -0,0 +1,10 @@
+# Python Learning Journey
+
+## Description
+This repository documents my journey of learning Python. I will regularly update this README with what I've learned.
+
+## What I Learned Today
+Today, I learned: 09 Aug 2024
+- How to print messages in Python.
+- How to set values in variables.
+- How to perform addition, subtraction, multiplication, and division operations.
---
+++ day_1.py
@@ -0,0 +1,31 @@
+# Print
+print("Hello World!")
+
+
+# variable
+text = "Printing Through Variable"
+print(text)
+
+
+
+# More Variables
+value1 = 20
+value2 = 10
+value3 = 5
+print("I have taken three variables. They are", value1,(","),value2,("&"),value3)
+
+# Addition
+addition = value1 + value2 + value3
+print("value1, value2 and value3 addition result is", addition)
+
+# Subtraction
+subtraction = value1-value2
+print("value1 and value2 subtraction result is", subtraction)
+
+# Multiplication
+multiplication = value2 * value3
+print("value2 and value3 multiplication result is", multiplication)
+
+# Division
+division = value2 / value3
+print("value2 and value3 division result is", division)
